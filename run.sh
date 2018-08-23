fce0c4f4b191e001a0e63c37b109c5df3a16d455
Jerry Jaehwang Jung <tomtomjhj@gmail.com>
2018-08-22 22:37:22 -0500

    testing interface for ERC777 functions

--- run.sh
+++ run.sh
@@ -8,15 +8,15 @@
   krun --smt none tests/name.erc777 -o none
   krun --smt none tests/symbol.erc777 -o none
   krun --smt none tests/granularity.erc777 -o none
-  krun --smt none tests/defaultOperators.erc777 # -o none
+  krun --smt none tests/defaultOperators.erc777 -o none
 
   krun --smt none tests/totalSupply.erc777 -o none
   krun --smt none tests/balanceOf.erc777 -o none
 
   krun --smt none tests/isOperatorFor.erc777 -o none
-  krun --smt none tests/authorizeOperator.erc777 # -o none
-  krun --smt none tests/revokeOperator.erc777 # -o none
+  krun --smt none tests/authorizeOperator.erc777 -o none
+  krun --smt none tests/revokeOperator.erc777 -o none
 
-  krun --smt none tests/send.erc777 # -o none
-  krun --smt none tests/operatorSend.erc777 # -o none
-  krun --smt none tests/operatorSend2.erc777 # -o none
+  krun --smt none tests/send.erc777 -o none
+  krun --smt none tests/operatorSend.erc777 -o none
+  krun --smt none tests/operatorSend2.erc777 -o none
